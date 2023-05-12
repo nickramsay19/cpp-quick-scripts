30b23f4b1cd15cefbd0d219b2eab01299d2b41b5
nickramsay19 <nickramsay19@gmail.com>
2023-05-12 22:17:07 +1000

    began parsing tasks.toml

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,26 @@
-#include <vector>
 #include <iostream>
+#include <vector>
+#include <string>
+#include <string_view>
+#include <toml.hpp>
 
 int main() {
-    std::cout << "yo\n";
+    
+    toml::table table = toml::parse_file("tasks.toml");
+
+    // convert scripts node to a toml::table
+    const toml::node_view scripts_node_view = table["scripts"];
+    const toml::table* scripts = scripts_node_view.as_table();
+
+    // loop over each script config
+    for (auto&& [key, val] : *scripts) {
+        const std::string_view script_name = key;
+        const toml::node& script_config = val;
+        std::cout << script_name << " : "\n";
+
+        
+    }
+    
+
     return 0;
 }
-
